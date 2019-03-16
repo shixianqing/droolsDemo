f4b5186878af7d537c6553c50ae1fbd5e7b4c7e9
shixianqing <1434501995@qq.com>
2019-03-16 16:47:55 +0800

    declare声明类型修改

---
+++ src/main/java/com/drools/chapter2/DeclareTest.java
@@ -0,0 +1,27 @@
+package com.drools.chapter2;
+
+import org.junit.Test;
+
+/**
+ * @Author:sxq
+ * @Date: 2019/3/16
+ * @Description:
+ * 适用于直接定义规则引擎的数据原型，不用再在java中创建。
+ * 新构建扩展原型
+ *
+ * declare Address
+ *     number : int
+ *     streetName : String
+ *     city : String
+ * end
+ */
+public class DeclareTest {
+
+
+    @Test
+    public void test(){
+
+    }
+}
+
+
